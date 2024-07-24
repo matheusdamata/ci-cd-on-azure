883eb870042f51b858cabcf19bfe47349d302fe1
Matheus Da Mata <matheusdamatag@gmail.com>
2024-07-24 13:06:23 -0300

    feat: add error handling for ZodError instance

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import fastify from 'fastify'
 import { checkoutRoutes } from './http/routes/checkout/routes'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
 app.register(checkoutRoutes, {
   prefix: '/checkout',
+})
+
+app.setErrorHandler((err, _, reply) => {
+  if (err instanceof ZodError)
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: err.format(),
+    })
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
 })
